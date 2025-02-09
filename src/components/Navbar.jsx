651477b1e63c092859ef7980748bbb66cbd6f8d6
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-09 22:06:32 +0500

    navbar is responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,37 @@
 function Navbar() {
   return (
     <div className="">
-    <div className=" p-5 flex justify-between  bg-blue-300 w-full    ">
+    <div className=" p-5 flex justify-between  bg-blue-300 w-full   ">
 
       <div className="">
         <h1>Faisal-Akram</h1>
       </div>
 
-      <div className="flex gap-5">
+      <div className="hidden sm:flex gap-5">
         <a href="about">About</a>
         <a href="#skill">Skills</a>
         <a href="#about">Projects</a>
         <a href="contact">Contact</a>
 
       </div>
+
+
+
+      <div className="sm:hidden">
+        <div className="flex flex-col ">
+        <a href="about">About</a>
+        <a href="#skill">Skills</a>
+        <a href="#about">Projects</a>
+        <a href="contact">Contact</a>
+        </div>
+   
+
+      </div>
     </div>
     </div>
+
+
+
   );
 }
 

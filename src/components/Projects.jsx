bfcea012e5a50c3cf3c31385f704a3f35ac0cd94
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-11 00:24:55 +0500

    projects component is responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,11 +5,12 @@ const Projects = () => {
 
     <>
      <h1 id="projects" className="text-center mt-10 mb-5 font-extrabold  ">Projects</h1>
+    
 
-    <div className=" flex justify-evenly ">
+    <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row justify-evenly items-center">
 
 
-      <div className="group border border-black w-96 ">
+      <div className="group border border-x-red-900 w-96 ">
         <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
           <img
             src={port}
@@ -80,27 +81,7 @@ const Projects = () => {
 
 
 
-    <div className="mt-10 flex justify-evenly ">
-
-
-<div className="group border border-black w-96 ">
-  <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
-    <img
-      src={port}
-      alt="project-img"
-      className="border border-black h-fit"
-    />
-
-    <div className=" absolute inset-0 flex justify-center items-center">
-      <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
-        <a href="https://react-web-plum.vercel.app/">View SIte</a>
-      </button>
-    </div>
-  </div>
-</div>
-
-
-
+    <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row justify-evenly items-center mt-10">
 
 <div className="group border border-black w-96 ">
   <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
@@ -136,10 +117,184 @@ const Projects = () => {
     </div>
   </div>
 </div>
+
+
+
+
+<div className="group border border-black w-96 ">
+  <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+    <img
+      src={port}
+      alt="project-img"
+      className="border border-black h-fit"
+    />
+
+    <div className=" absolute inset-0 flex justify-center items-center">
+      <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+        <a href="https://react-web-plum.vercel.app/">View SIte</a>
+      </button>
+    </div>
+  </div>
 </div>
+</div>
+
+
+
+
+
+
+
+
+{/* Mobile screens */}
+
+
+{/* <h1 id="projects" className=" sm:hidden text-center mt-10 mb-5 font-extrabold  ">Projects</h1> */}
+{/* 
+<div className=" sm:hidden flex flex-col justify-evenly gap-5  ">
+
+
+  <div className="group border border-black w-96 ">
+    <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+      <img
+        src={port}
+        alt="project-img"
+        className="border border-black h-fit"
+      />
+
+      <div className=" absolute inset-0 flex justify-center items-center">
+        <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+          <a href="https://react-web-plum.vercel.app/">View SIte</a>
+        </button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <div className="group border border-black w-96 ">
+    <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+      <img
+        src={port}
+        alt="project-img"
+        className="border border-black h-fit"
+      />
+
+      <div className=" absolute inset-0 flex justify-center items-center">
+        <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+          <a href="https://react-web-plum.vercel.app/">View SIte</a>
+        </button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <div className="group border border-black w-96 ">
+    <div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+      <img
+        src={port}
+        alt="project-img"
+        className="border border-black h-fit"
+      />
+
+      <div className=" absolute inset-0 flex justify-center items-center">
+        <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+          <a href="https://react-web-plum.vercel.app/">View SIte</a>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="sm:hidden mt-10 flex flex-col justify-center gap-5  ">
+
+
+<div className="group border border-black w-96 ">
+<div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+<img
+  src={port}
+  alt="project-img"
+  className="border border-black h-fit"
+/>
+
+<div className=" absolute inset-0 flex justify-center items-center">
+  <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+    <a href="https://react-web-plum.vercel.app/">View SIte</a>
+  </button>
+</div>
+</div>
+</div>
+
+
+
+
+<div className="group border border-black w-96 ">
+<div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+<img
+  src={port}
+  alt="project-img"
+  className="border border-black h-fit"
+/>
+
+<div className=" absolute inset-0 flex justify-center items-center">
+  <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+    <a href="https://react-web-plum.vercel.app/">View SIte</a>
+  </button>
+</div>
+</div>
+</div>
+
+
+
+
+<div className="group border border-black w-96 ">
+<div className=" p-2 w-96 border border-black relative flex justify-center items-center  group-hover:bg-red-500 ">
+<img
+  src={port}
+  alt="project-img"
+  className="border border-black h-fit"
+/>
+
+<div className=" absolute inset-0 flex justify-center items-center">
+  <button className=" opacity-0 group-hover:opacity-100 group-hover:border group-hover:h-8 group-hover:w-20  group-hover:bg-red-400 group-hover:rounded-md group-hover:text-white  ">
+    <a href="https://react-web-plum.vercel.app/">View SIte</a>
+  </button>
+</div>
+</div>
+</div>
+</div>
+
+ */}
+
+
+
+
+
+
     </>
   );
 };
+
+
+
 
 export default Projects;
 
